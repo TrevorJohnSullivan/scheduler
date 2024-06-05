2fcd684d8f44c5db49b02e8e7c581b7d30a9e0f2
Trevor Sullivan <trevorjohnsullivan@gmail.com>
2024-06-05 17:10:58 -0230

    completed Adding Transition activity

---
+++ src/hooks/useVisualMode.js
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+function useVisualMode(initial) {
+  const [mode, setMode] = useState(initial);
+
+  function transition(newMode){
+    setMode(newMode)
+  }
+
+  return { mode, transition };
+}
+
+export default useVisualMode;
