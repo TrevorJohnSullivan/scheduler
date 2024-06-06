97eb9f5c776c9b72fdb1bae9018612d03e1843c9
Trevor Sullivan <trevorjohnsullivan@gmail.com>
2024-06-06 12:25:07 -0230

    completed activity Adding back

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -1,13 +1,17 @@
 import { useState } from "react";
 
 function useVisualMode(initial) {
-  const [mode, setMode] = useState(initial);
+  const [history, setHistory] = useState([initial]); 
 
   function transition(newMode){
-    setMode(newMode)
+    setHistory(prev => [...prev, newMode])
   }
 
-  return { mode, transition };
+  function back() {
+    setHistory(prev => [...prev.slice(0, prev.length - 1)])
+  }
+
+  return { mode: history[history.length -1], transition, back };
 }
 
 export default useVisualMode;
